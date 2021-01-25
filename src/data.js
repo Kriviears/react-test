b80a7bbe5a38d92c2de28e02ebf7692ebadb3946
Khari Riviears <kriviears@gmail.com>
2021-01-25 18:33:56 -0500

    changed the order of projects

--- src/data.js
+++ src/data.js
@@ -1,6 +1,13 @@
 export default{ 
     "projects": [
         {
+            name: "Fighting game frame data app",
+            githubPage: "https://github.com/Kriviears/granblue-frame-api",
+            deployment: "https://believeinframes.now.sh/",
+            preview: "https://upload.wikimedia.org/wikipedia/en/9/93/Granblue_Fantasy_Versus_logo.png",
+            description: "Personal project to easily look up and compare the animation frames for competitive gaming purposes. Full stack app made in React and custom api made with Nodejs and backend database stored in postgreSQL"
+        },
+        {
             name: "Star Trek Themed Quiz",
             githubPage: "https://github.com/Kriviears/quiz-app",
             deployment: "https://kriviears.github.io/quiz-app/",
@@ -20,13 +27,6 @@ export default{
             deployment: "https://noteful-app.kriviears.now.sh/",
             preview: "https://cdn.discordapp.com/attachments/656305842563579914/656305869667041281/9c4edb0d-753b-44f7-9c11-56a892988ed2.png",
             description: "Take notes, save them in folders and come back to it later. Full stack app made in React and custom api made with Nodejs and backend database stored in postgreSQL"
-        },
-        {
-            name: "Samurai Shodown frame data app",
-            githubPage: "https://github.com/Kriviears/T7AppPc",
-            deployment: "www.github.com/samurai",
-            preview: "",
-            description: "Personal project to easily look up and compare the animation frames for competitive gaming purposes. Full stack app made in React and custom api made with Nodejs and backend database stored in postgreSQL"
         },
     ]
 }
